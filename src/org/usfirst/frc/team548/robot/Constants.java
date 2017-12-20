4cc792ae0fcf1d76b8ae0bc20aef1808b0b2c3b6
Dale Van Voorst <dale@dalevanvoorst.com>
2017-12-19 19:18:14 -0500

    recalibrate turn encoders

--- src/org/usfirst/frc/team548/robot/Constants.java
+++ src/org/usfirst/frc/team548/robot/Constants.java
@@ -7,24 +7,22 @@ public class Constants {
 	 * Drive Train
 	 */
 	
-	// NEED TO RELABEL PHYSICAL MODULES TO MATCH CODE
-	
-	// Physical Module A
-	public final static int DT_D_DRIVE_TALON_ID = 7;
-	public final static int DT_D_TURN_TALON_ID = 8;
-	public final static double DT_D_ABS_ZERO = 0.532;
-	//Physical Module - B
+	// Physical Module - A
 	public final static int DT_A_DRIVE_TALON_ID = 1;
 	public final static int DT_A_TURN_TALON_ID = 2;
-	public final static double DT_A_ABS_ZERO = .827;
-	//Physical Module - C
+	public final static double DT_A_ABS_ZERO = .834;
+	// Physical Module - B
+	public final static int DT_B_DRIVE_TALON_ID = 5;
+	public final static int DT_B_TURN_TALON_ID = 6;
+	public final static double DT_B_ABS_ZERO = .902;
+	// Physical Module - C
 	public final static int DT_C_DRIVE_TALON_ID = 3;
 	public final static int DT_C_TURN_TALON_ID = 4;
 	public final static double DT_C_ABS_ZERO = .9;
-	//Physical Module - D
-	public final static int DT_B_DRIVE_TALON_ID = 5;
-	public final static int DT_B_TURN_TALON_ID = 6;
-	public final static double DT_B_ABS_ZERO = .913;
+	// Physical Module - D
+	public final static int DT_D_DRIVE_TALON_ID = 7;
+	public final static int DT_D_TURN_TALON_ID = 8;
+	public final static double DT_D_ABS_ZERO = 0.526;
 	
 	//Rot PID .01, 0.0001, 0.008,
 	public final static double DT_ROT_PID_P = .007;
