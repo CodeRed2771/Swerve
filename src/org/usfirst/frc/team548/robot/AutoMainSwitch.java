8f20e3bceab244251b4fdadc88061f8afe1335a9
Dale Van Voorst <dale@dalevanvoorst.com>
2018-02-08 19:51:57 -0500

    Auto work

--- src/org/usfirst/frc/team548/robot/AutoMainSwitch.java
+++ src/org/usfirst/frc/team548/robot/AutoMainSwitch.java
@@ -2,39 +2,57 @@ package org.usfirst.frc.team548.robot;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-public class AutoMainSwitch extends AutoBaseClass{
+public class AutoMainSwitch extends AutoBaseClass {
 	AutoBaseClass mAutoSubroutine;
-	
+
 	public AutoMainSwitch(int robotPosition) {
 		super(robotPosition);
-	}	
-	
+	}
+
 	public void tick() {
 		if (isRunning()) {
-			
-			SmartDashboard.putNumber("Auto Base Step", getCurrentStep());
-			
-			switch(getCurrentStep()) {
-			case 0:
-				mAutoSubroutine = new AutoStartToSwitch(this.robotPosition());
-				mAutoSubroutine.start();
-				advanceStep();
-				break;
-			case 1:
-				if (mAutoSubroutine.hasCompleted())
+
+			if (robotPosition() == 2) {
+				switch (getCurrentStep()) {
+				case 0:
+					mAutoSubroutine = new AutoBaseLine(this.robotPosition());
+					mAutoSubroutine.start();
+					break;
+				case 3:
+					if (mAutoSubroutine.hasCompleted())
+						stop();
+					break;
+				case 4:
+					break;
+				}
+			} else {
+
+				SmartDashboard.putNumber("Auto Base Step", getCurrentStep());
+
+				switch (getCurrentStep()) {
+				case 0:
+					mAutoSubroutine = new AutoStartToSwitch(this.robotPosition());
+					mAutoSubroutine.start();
 					advanceStep();
-				break;
-			case 2:
-				mAutoSubroutine = new AutoSwitchPlaceCube(this.robotPosition());
-				mAutoSubroutine.start();
-				advanceStep();
-				break;
-			case 3:
-				if (mAutoSubroutine.hasCompleted()) 
-					stop();
-				break;
-			case 4:
-				break;			}
+					break;
+				case 1:
+					if (mAutoSubroutine.hasCompleted())
+						advanceStep();
+					break;
+				case 2:
+					mAutoSubroutine = new AutoSwitchPlaceCube(this.robotPosition());
+					mAutoSubroutine.start();
+					advanceStep();
+					break;
+				case 3:
+					if (mAutoSubroutine.hasCompleted())
+						stop();
+					break;
+				case 4:
+					break;
+				}
+			}
 		}
+
 	}
 }
